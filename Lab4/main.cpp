33becbb88653ef9e3232a1f7cb3bb25195d2a155
AbdelrahmanMosly <abdelrahman.b.mosly@gmail.com>
2023-04-10 22:48:49 +0200

    bresenham implemented

--- Lab4/main.cpp
+++ Lab4/main.cpp
@@ -12,75 +12,88 @@ using namespace std;
 #define DDA_INDEX 0
 #define BRESENHAM_INDEX 1
 
-#define DDA_YOFFSET 5
 #define DDA_XOFFSET 5
+#define DDA_YOFFSET 5
+
+#define BRESENHAM_XOFFSET 5
+#define BRESENHAM_YOFFSET 25
 
 
-int pointsCtr=0;
+int pointsCtr = 0;
 static GLfloat name_start_end[][4] =
         {
-                {0, 0, 5, 10},
-                { 5, 10, 10, 0},
-                {4, 5, 7, 5}, //A
+                {0,  0,  5,  10},
+                {5,  10, 10, 0},
+                {4,  5,  7,  5}, //A
 
-                {12, 0, 12, 10},
-                {12, 0, 17, 1},
-                {12, 5, 17, 4},
-                {17, 1, 17, 4}, //b
+                {12, 0,  12, 10},
+                {12, 0,  17, 1},
+                {12, 5,  17, 4},
+                {17, 1,  17, 4}, //b
 
-                {19, 1, 19, 4},
-                {19, 0, 24, 1},
-                {19, 5, 24, 4},
-                {24, 0, 24, 10}, //d
+                {19, 1,  19, 4},
+                {19, 0,  24, 1},
+                {19, 5,  24, 4},
+                {24, 0,  24, 10}, //d
 
-                {26, 0, 26, 10},
-                {26, 0, 31, 0},
-                {26, 5, 31, 5},
+                {26, 0,  26, 10},
+                {26, 0,  31, 0},
+                {26, 5,  31, 5},
                 {26, 10, 31, 10},
                 {31, 10, 31, 5},//e
 
-                {33, 0, 33, 10}, // l
+                {33, 0,  33, 10}, // l
 
-                {35, 0, 35, 10},
-                {35, 9, 40, 9}, // r
+                {35, 0,  35, 10},
+                {35, 9,  40, 9}, // r
 
-                {42, 5, 42, 0},
-                {42, 0, 47, 0},
-                {42, 0, 47, 0},
-                {42, 5, 47, 5},
+                {42, 5,  42, 0},
+                {42, 0,  47, 0},
+                {42, 0,  47, 0},
+                {42, 5,  47, 5},
                 {42, 10, 47, 10},
-                {47, 0, 47, 10},//a
+                {47, 0,  47, 10},//a
 
-                {49, 0, 49, 10},
-                {49, 6, 54, 5},
-                {54, 0, 54, 5}, // h
+                {49, 0,  49, 10},
+                {49, 6,  54, 5},
+                {54, 0,  54, 5}, // h
 
 
-                {56, 0, 56, 5},
-                {56, 5, 61, 5},
-                {61, 0, 61, 5},
-                {61, 5, 66, 5},
-                {66, 0, 66, 5},// m
+                {56, 0,  56, 5},
+                {56, 5,  61, 5},
+                {61, 0,  61, 5},
+                {61, 5,  66, 5},
+                {66, 0,  66, 5},// m
 
-                {68, 5, 68, 0},
-                {68, 0, 73, 0},
-                {68, 5, 73, 5},
+                {68, 5,  68, 0},
+                {68, 0,  73, 0},
+                {68, 5,  73, 5},
                 {68, 10, 73, 10},
-                {73, 0, 73, 10}, // a
+                {73, 0,  73, 10}, // a
 
-                {75, 0, 75, 5},
-                {75, 5, 80, 5},
-                {80, 0, 80, 5},//n
+                {75, 0,  75, 5},
+                {75, 5,  80, 5},
+                {80, 0,  80, 5},//n
 
         };
 
-//Define DDA
 vector<GLfloat> DDA(float x1, float y1, float x2, float y2) {
 
-    x1+=DDA_XOFFSET;
-    y1+=DDA_YOFFSET;
-    x2+=DDA_XOFFSET;
-    y2+=DDA_YOFFSET;
+    x1 += DDA_XOFFSET;
+    y1 += DDA_YOFFSET;
+    x2 += DDA_XOFFSET;
+    y2 += DDA_YOFFSET;
+    if(x2<x1)
+    {
+        int temp=x1;
+        x1=x2;
+        x2=temp;
+        temp=y1;
+        y1=y2;
+        y2=temp;
+    }
+
+
     int dx = x2 - x1;
     int dy = y2 - y1;
     float steps = max(abs(dx), abs(dy));
@@ -103,6 +116,59 @@ vector<GLfloat> DDA(float x1, float y1, float x2, float y2) {
     return vertices;
 }
 
+vector<GLfloat> bresenham(int x1, int y1, int x2, int y2) {
+    x1 += BRESENHAM_XOFFSET;
+    y1 += BRESENHAM_YOFFSET;
+    x2 += BRESENHAM_XOFFSET;
+    y2 += BRESENHAM_YOFFSET;
+    int dx = x2 - x1;
+    int dy = y2 - y1;
+
+    int x = x1;
+    int y = y1;
+
+
+    int steps = max(abs(dx), abs(dy));
+    vector<GLfloat> vertices((steps + 1) * 3);
+
+    // -1 <slope m <1
+    if (abs(dx) > abs(dy)) {
+        int pk = 2 * abs(dy) - abs(dx);
+
+        for (int i = 0; i <= steps; i++) {
+            vertices[i * 3] = x; //x
+            vertices[i * 3 + 1] = y; //y
+            vertices[i * 3 + 2] = 0; //z
+
+            x += dx>0?  1:  -1;
+            if (pk < 0) {
+                pk = pk + 2 * (abs(dy));
+            }else{
+                y += dy>0? 1 : -1;
+                pk = pk + 2 * (abs(dy) -abs(dx)) ;
+            }
+        }
+    } else {
+        int pk = 2 * abs(dx) - abs(dy);
+
+        for (int i = 0; i <= steps; i++) {
+            vertices[i * 3] = x; //x
+            vertices[i * 3 + 1] = y; //y
+            vertices[i * 3 + 2] = 0; //z
+
+            y += dy>0? 1 : -1;
+            if (pk < 0) {
+                pk = pk + 2 * (abs(dx));
+            }else{
+                x += dx>0?  1:  -1;
+                pk = pk + 2 * (abs(dx) -abs(dy)) ;
+            }
+        }
+    }
+    return vertices;
+
+
+}
 
 static unsigned int buffer[2]; // Array of buffer ids.
 
@@ -116,27 +182,25 @@ void drawScene(void) {
     // Draw annulus.
     glBindVertexArray(vao[DDA_INDEX]);
     glPointSize(5.0f);
-    glDrawArrays(GL_POINTS, 0, pointsCtr/3);
+    glDrawArrays(GL_POINTS, 0, pointsCtr / 3);
 
     glFlush();
 }
 
 // Initialization routine.
 void setup(void) {
-    vector<GLfloat> name;
+    vector<GLfloat> nameDDA;
 
     int size_name_points = sizeof(name_start_end) / sizeof(name_start_end[0]);
     for (int i = 0; i < size_name_points; i++) {
         std::vector<GLfloat> line = DDA(name_start_end[i][0], name_start_end[i][1],
-                                               name_start_end[i][2], name_start_end[i][3]);
+                                        name_start_end[i][2], name_start_end[i][3]);
         // Loop through the returned vector and add each element to the final result vector
         for (int j = 0; j < line.size(); j++) {
             pointsCtr++;
-            name.push_back(line[j]);
+            nameDDA.push_back(line[j]);
         }
     }
-
-
 
 
     glClearColor(1.0, 1.0, 1.0, 0.0);
@@ -149,7 +213,7 @@ void setup(void) {
 
     // Bind vertex buffer and reserve space.
     glBindBuffer(GL_ARRAY_BUFFER, buffer[VERTICES]);
-    glBufferData(GL_ARRAY_BUFFER, name.size() * sizeof(GLfloat), name.data(), GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, nameDDA.size() * sizeof(GLfloat), nameDDA.data(), GL_STATIC_DRAW);
 
 //    // Copy vertex coordinates data into first half of vertex buffer.
 //    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices1), vertices1);
